c8bd0a5459ac9a7625b69f857ed84135bf63c941
steven victor <chikodi543@gmail.com>
2019-11-06 13:35:33 +0100

    Test without env

--- tests/setup_test.go
+++ tests/setup_test.go
@@ -33,7 +33,7 @@ func Database() {
 
 	var err error
 
-	TestDbDriver := os.Getenv("TEST_DB_DRIVER")
+	// TestDbDriver := os.Getenv("TEST_DB_DRIVER")
 
 	// if TestDbDriver == "mysql" {
 	// 	DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("TEST_DB_USER"), os.Getenv("TEST_DB_PASSWORD"), os.Getenv("TEST_DB_HOST"), os.Getenv("TEST_DB_PORT"), os.Getenv("TEST_DB_NAME"))
