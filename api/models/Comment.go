c028be0dda67949f39a66d9aca6cdb88f0109cda
steven victor <chikodi543@gmail.com>
2019-11-09 10:48:38 +0100

    Increase the string length for comment body

--- api/models/Comment.go
+++ api/models/Comment.go
@@ -14,7 +14,7 @@ type Comment struct {
 	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
 	UserID    uint32    `gorm:"not null" json:"user_id"`
 	PostID    uint64    `gorm:"not null" json:"post_id"`
-	Body      string    `gorm:"not null" json:"body"`
+	Body      string    `gorm:"size:999999;not null;" json:"body"`
 	User      User      `json:"user"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
