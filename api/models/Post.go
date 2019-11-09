bf9e0a321cab569cf6b667eeea319748dce43716
steven victor <chikodi543@gmail.com>
2019-11-09 10:46:59 +0100

    Increase the string length for post content

--- api/models/Post.go
+++ api/models/Post.go
@@ -12,7 +12,7 @@ import (
 type Post struct {
 	ID        uint64    `gorm:"primary_key;auto_increment" json:"id"`
 	Title     string    `gorm:"size:255;not null;unique" json:"title"`
-	Content   string    `gorm:"size:500;not null;" json:"content"`
+	Content   string    `gorm:"size:999999;not null;" json:"content"`
 	Author    User      `json:"author"`
 	AuthorID  uint32    `gorm:"not null" json:"author_id"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
